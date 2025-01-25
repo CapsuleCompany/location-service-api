b3b6ac627f13d9e2744e2d872271af1016e6cc8d
Camryn Crowder <camryn.crowder@apkudo.com>
2025-01-25 12:53:17 -0500

    chore: update settings for drf

--- core/settings.py
+++ core/settings.py
@@ -63,14 +63,6 @@ DATABASES = {
     }
 }
 
-# Password validation
-AUTH_PASSWORD_VALIDATORS = [
-    {"NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"},
-    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator"},
-    {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator"},
-    {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"},
-]
-
 # Internationalization
 LANGUAGE_CODE = "en-us"
 TIME_ZONE = "UTC"
